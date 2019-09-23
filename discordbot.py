9fffe24193d56207986e54875b9a16a80850c687
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-24 02:33:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -32,7 +32,7 @@ async def pt(ctx, about = "募集", cnt = 4, settime = 10.0):
         else:
             return emoji == '⏫' or emoji == '✖'
 
-     while cnt:
+     while 0 < cnt:
         try:
             reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
         except asyncio.TimeoutError:
