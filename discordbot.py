be5db4cc33c646ebe31fdc15e627c1e110de438f
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-24 02:56:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,58 +7,64 @@ client = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@client.command()
-async def pt(ctx, about = "募集", cnt = 8, settime = 3600.0):
-    cnt, settime = int(cnt), float(settime)
-    cntb = cnt
-    reaction_member = ["下記は参加者一覧です。↑で参加表明、↓で離脱が行えます。集まらなかった場合も1時間後に募集は破棄されます。"]
-    test = discord.Embed(title=about,colour=0xffffff)
-    test.add_field(name=f"{cntb}人PTで残り{cnt}人募集\n", value="下記は参加者一覧です。↑で参加表明、↓で離脱が行えます。集まらなかった場合も1時間後に募集は破棄されます。", inline=True)
-    msg = await ctx.send(embed=test)
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
+@client.command()
+async def pt(ctx, about = "募集", cnt = 4, settime = 10.0):
+    cnt, settime = int(cnt), float(settime)
+    reaction_member = [""]
+    test = discord.Embed(title=about,colour=0x1e90ff)
+    test.add_field(name=f"あと{cnt}人 募集中\n", value=None, inline=True)
+    msg = await ctx.send(embed=test)
+    #投票の欄
     await msg.add_reaction('⏫')
     await msg.add_reaction('✖')
 
     def check(reaction, user):
         emoji = str(reaction.emoji)
-        if user.bot == True:
+        if user.bot == True:    # botは無視
             pass
         else:
             return emoji == '⏫' or emoji == '✖'
 
-    while len(reaction_member)-1 <= 8:
+    while 0 < cnt:
         try:
             reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
         except asyncio.TimeoutError:
-            await ctx.send('時間になったので'+about+'は〆ます。')
+            await ctx.send('残念、人が足りなかったようだ...')
             break
         else:
             print(str(reaction.emoji))
             if str(reaction.emoji) == '⏫':
                 reaction_member.append(user.name)
                 cnt -= 1
-                test = discord.Embed(title=about,colour=0xffffff)
-                test.add_field(name=f"{cntb}人PTで残り{cnt}人募集\n", value='\n'.join(reaction_member), inline=True)
+                test = discord.Embed(title=about,colour=0x1e90ff)
+                test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
                 await msg.edit(embed=test)
+
                 if cnt == 0:
-                    test = discord.Embed(title=about,colour=0xffffff)
-                    test.add_field(name=f"{cntb}人PTで残り{cnt}人募集\n", value='\n'.join(reaction_member), inline=True)
-
+                    test = discord.Embed(title=about,colour=0x1e90ff)
+                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
                     await msg.edit(embed=test)
-
-                    await ctx.send('メンバーが集まったので'+about+'は〆ます。')
-                    break
+                    finish = discord.Embed(title=about,colour=0x1e90ff)
+                    finish.add_field(name="おっと、メンバーがきまったようだ",value='\n'.join(reaction_member), inline=True)
+                    await ctx.send(embed=finish)
 
             elif str(reaction.emoji) == '✖':
                 if user.name in reaction_member:
                     reaction_member.remove(user.name)
                     cnt += 1
-                    test = discord.Embed(title=about,colour=0xffffff)
-                    test.add_field(name=f"{cntb}人PTで残り{cnt}人募集\n", value='\n'.join(reaction_member), inline=True)
+                    test = discord.Embed(title=about,colour=0x1e90ff)
+                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
                     await msg.edit(embed=test)
                 else:
                     pass
-
+        # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
         await msg.remove_reaction(str(reaction.emoji), user)
 
 client.run(token)
