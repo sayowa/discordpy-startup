ec7fe38141efd4ae1a83947ca866d69cd79a7efa
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-24 01:21:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
@@ -64,5 +64,6 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
                     pass
         # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
         await msg.remove_reaction(str(reaction.emoji), user)
+
 
 bot.run(token)
