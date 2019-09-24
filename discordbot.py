d0b415d9cd91fc4c58255ab1f293caab26717417
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-24 14:03:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,9 @@ async def on_ready():
     print('------')
 
 @client.command()
+ if message.content == 'pthelp':
+        await message.channel.send('【募集名】・【募集人数】・【募集リミット（分）】になってるｿﾞ！\n『・』は、半角スペースで頼むｿﾞ\n
+        募集名以降は記載しない場合、デフォで人数は4人、リミットは30分ｿﾞ')
 async def pt(ctx, about = "募集", cnt = 4, settime = 30.0):
     cnt, settime = int(cnt), float(settime*60)
     reaction_member = ["参加者一覧"]
