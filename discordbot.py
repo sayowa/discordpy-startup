5b1d5adae6115b4091f96b151d3230508a438a0c
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-26 21:33:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -68,12 +68,11 @@ async def pt(ctx, about = "募集", cnt = 4, settime = 30.0):
                 else:
                     pass
             elif str(reaction.emoji) == '🚫':
-                test = discord.Embed(title=about,colour=0x1e90ff)
-                test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
-                await msg.edit(embed=test)
-                finish = discord.Embed(title=about,colour=0x1e90ff)
-                finish.add_field(name="メンバーがきまったようｿﾞ\n復活の魂わすれるなよ！", value='\n'.join(reaction_member),inline=True)
-                await ctx.send(embed=finish)
+                reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send('募集時間が過ぎたｿﾞ、再度、募集ｵﾅｼｬｽ!')
+            break
+        else:
         # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
         await msg.remove_reaction(str(reaction.emoji), user)
             
