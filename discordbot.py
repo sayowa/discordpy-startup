65d2f0e0d6050bb5c198ff407fba3abe163a54a5
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-26 20:53:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,7 +30,7 @@ async def pt(ctx, about = "募集", cnt = 4, settime = 30.0):
         if user.bot == True:    # botは無視
             pass
         else:
-            return emoji == '⏫' or emoji == '⏬'
+            return emoji == '⏫' or emoji == '⏬' or emoji == '🚫'
 
     while 0 <= cnt:
         try:
@@ -57,6 +57,7 @@ async def pt(ctx, about = "募集", cnt = 4, settime = 30.0):
                     finish = discord.Embed(title=about,colour=0x1e90ff)
                     finish.add_field(name="メンバーがきまったようｿﾞ\n復活の魂わすれるなよ！", value='\n'.join(reaction_member),inline=True)
                     await ctx.send(embed=finish)
+                                     
                     
             elif str(reaction.emoji) == '⏬':
                 if user.name in reaction_member:
@@ -67,6 +68,15 @@ async def pt(ctx, about = "募集", cnt = 4, settime = 30.0):
                     await msg.edit(embed=test)
                 else:
                     pass
+                
+                elif cnt == 0:
+                    test = discord.Embed(title=about,colour=0x1e90ff)
+                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
+                    await msg.edit(embed=test)
+                    finish = discord.Embed(title=about,colour=0x1e90ff)
+                    finish.add_field(name="メンバーがきまったようｿﾞ\n復活の魂わすれるなよ！", value='\n'.join(reaction_member),inline=True)
+                    await ctx.send(embed=finish)
+                    
         # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
         await msg.remove_reaction(str(reaction.emoji), user)
             
