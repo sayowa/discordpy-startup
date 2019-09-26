0ac305b5b232754c5a31665b4ec64108b1ad8be3
小夜和 <55704352+sayowa@users.noreply.github.com>
2019-09-26 20:10:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -58,7 +58,7 @@ async def pt(ctx, about = "募集", cnt = 4, settime = 30.0):
                     finish.add_field(name="メンバーがきまったようｿﾞ\n復活の魂わすれるなよ！", value='\n'.join(reaction_member),inline=True)
                     await ctx.send(embed=finish)
                     
-                elif str(reaction.emoji) = '🚫':
+            elif str(reaction.emoji) = '🚫':
                     test = discord.Embed(title=about,colour=0x1e90ff)
                     test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
                     await msg.edit(embed=test)
